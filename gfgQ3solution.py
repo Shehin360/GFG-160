34fd9e4de6fe7a523dc5df4361af9a47d69a570a
Shehin T Shaji <shehin@Shehins-MacBook-Air.local>
2025-07-25 17:51:03 +0530

    Completed third question

---
+++ gfgQ3solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseArray(self, arr):
+        # code here
+        print(arr.reverse())
+        
+        
+        
+
+
+
+
+
+# my_list = [1, 2, 3, 4]
+# reversed_list = my_list[::-1]
+# print(reversed_list)
